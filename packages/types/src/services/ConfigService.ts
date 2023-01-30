a3a5eace30149dcce75e472078c4be6cfc09019e
homura <homura.dev@gmail.com>
2023-01-30 20:05:36 +0800

    refactor: simply method name
    Signed-off-by: homura <homura.dev@gmail.com>

--- packages/types/src/services/ConfigService.ts
+++ packages/types/src/services/ConfigService.ts
@@ -2,17 +2,18 @@ import { Promisable } from '../base';
 
 export interface ConfigService {
   getConfig(): Promisable<Config>;
+  setConfig(payload: { config: Partial<Config> }): Promisable<void>;
 
-  updateConfig(payload: { config: Partial<Config> }): Promisable<void>;
-
-  getAvailableNetworks(): Promisable<NetworkConfig[]>;
-  updateNetwork(payload: { network: NetworkConfig }): Promisable<void>;
-  switchNetwork(payload: { id: string }): Promisable<void>;
+  getNetworks(): Promisable<NetworkConfig[]>;
+  addNetwork(payload: { network: NetworkConfig }): Promisable<void>;
   removeNetwork(payload: { id: string }): Promisable<void>;
+  getSelectedNetwork(): Promisable<NetworkConfig>;
+  setSelectedNetwork(payload: { id: string }): Promisable<void>;
 
-  updateNickname(payload: { nickname: string }): Promisable<void>;
+  getNickname(): Promisable<string>;
+  setNickname(payload: { nickname: string }): Promisable<void>;
 
-  appendWhitelistItem(payload: { host: string }): Promisable<void>;
+  addWhitelistItem(payload: { host: string }): Promisable<void>;
   removeWhitelistItem(payload: { host: string }): Promisable<void>;
 
   getVersion(): Promisable<void>;
@@ -23,8 +24,18 @@ export interface Config {
    * version of the current app
    */
   version: string;
+  /**
+   * the nickname of the current user, for display purpose.
+   * unlike other wallet, Nexus don't use a certain address as the identity of the user
+   */
   nickname: string;
+  /**
+   * the id of networks that is selected to connect
+   */
   selectedNetwork: string;
+  /**
+   * a list of networks that the app can connect to
+   */
   networks: NetworkConfig[];
   /**
    * a list of hosts that have been granted
