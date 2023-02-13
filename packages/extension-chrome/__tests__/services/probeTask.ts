b5b37dd98c76e5021af695c20adef8c475359ea2
zhangyouxin <zhangyouxingg@126.com>
2023-02-13 11:24:45 +0800

    test: add test case for probe task sync all lock info

--- packages/extension-chrome/__tests__/services/probeTask.ts
+++ packages/extension-chrome/__tests__/services/probeTask.ts
@@ -12,36 +12,40 @@ import { ProbeTask } from '../../src/services/backend/probeTask';
 import { Storage } from '@nexus-wallet/types';
 import { getDefaultLocksAndPointer } from '../../src/services/backend/utils';
 
+let probeTask: ProbeTask;
+let memoryStorage: LockInfoStorage;
+let mockStorage: Storage<LockInfoStorage>;
+beforeAll(() => {
+  const mockBackend: Backend = createMockBackend({});
+  const mockKeystoreService = createMockKeystoreService({
+    getPublicKeyByPath: ({ path }) =>
+      [...mockFullOwnershipLockInfos, ...mockRuleBasedOwnershipLockInfos].find((info) => info.path === path)!.publicKey,
+  });
+  memoryStorage = {
+    fullOwnership: getDefaultLocksAndPointer(),
+    ruleBasedOwnership: getDefaultLocksAndPointer(),
+  };
+  mockStorage = {
+    setItem: jest.fn((key, value) => {
+      memoryStorage[key] = value;
+      return Promise.resolve();
+    }),
+    getItem: jest.fn((key) => {
+      return Promise.resolve(memoryStorage[key]);
+    }),
+    removeItem: jest.fn(),
+    hasItem: jest.fn(),
+  };
+
+  probeTask = ProbeTask.getInstance({
+    backend: mockBackend,
+    keystoreService: mockKeystoreService,
+    storage: mockStorage,
+  });
+});
+
 describe('probe task', () => {
   it('should supplyOffChainAddresses if not enough', async () => {
-    const mockBackend: Backend = createMockBackend({});
-    const mockKeystoreService = createMockKeystoreService({
-      getPublicKeyByPath: ({ path }) =>
-        [...mockFullOwnershipLockInfos, ...mockRuleBasedOwnershipLockInfos].find((info) => info.path === path)!
-          .publicKey,
-    });
-    const memoryStorage: LockInfoStorage = {
-      fullOwnership: getDefaultLocksAndPointer(),
-      ruleBasedOwnership: getDefaultLocksAndPointer(),
-    };
-    const mockStorage: Storage<LockInfoStorage> = {
-      setItem: jest.fn((key, value) => {
-        memoryStorage[key] = value;
-        return Promise.resolve();
-      }),
-      getItem: jest.fn((key) => {
-        return Promise.resolve(memoryStorage[key]);
-      }),
-      removeItem: jest.fn(),
-      hasItem: jest.fn(),
-    };
-
-    const probeTask = ProbeTask.getInstance({
-      backend: mockBackend,
-      keystoreService: mockKeystoreService,
-      storage: mockStorage,
-    });
-
     await probeTask.supplyOffChainAddresses({ keyName: 'fullOwnership' });
     expect(memoryStorage.fullOwnership).toEqual({
       details: {
@@ -73,39 +77,22 @@ describe('probe task', () => {
   });
 
   it('should run probe task', async () => {
-    const mockBackend: Backend = createMockBackend({});
-    const mockKeystoreService = createMockKeystoreService({
-      getPublicKeyByPath: ({ path }) =>
-        [...mockFullOwnershipLockInfos, ...mockRuleBasedOwnershipLockInfos].find((info) => info.path === path)!
-          .publicKey,
-    });
-    const memoryStorage: LockInfoStorage = {
-      fullOwnership: getDefaultLocksAndPointer(),
-      ruleBasedOwnership: getDefaultLocksAndPointer(),
-    };
-    const mockStorage: Storage<LockInfoStorage> = {
-      setItem: jest.fn((key, value) => {
-        memoryStorage[key] = value;
-        return Promise.resolve();
-      }),
-      getItem: jest.fn((key) => {
-        return Promise.resolve(memoryStorage[key]);
-      }),
-      removeItem: jest.fn(),
-      hasItem: jest.fn(),
-    };
-
-    const probeTask = ProbeTask.getInstance({
-      backend: mockBackend,
-      keystoreService: mockKeystoreService,
-      storage: mockStorage,
-    });
     expect(probeTask.running).toBe(false);
+    jest.useFakeTimers();
+    const logSpy = jest.spyOn(console, 'log');
     probeTask.run();
-    (() => {
-      return new Promise((resolve) => setTimeout(resolve, 3_000));
-    })();
+    // the second call will be ignored
+    probeTask.run();
+    jest.advanceTimersByTime(10_000);
+    expect(logSpy).toBeCalledWith('probe task running...');
     expect(probeTask.running).toBe(true);
     probeTask.stop();
+    jest.clearAllTimers();
+    logSpy.mockRestore();
+  });
+
+  it('should sync from scratch', async () => {
+    await probeTask.syncAllLocksInfo();
+    expect(mockStorage.setItem).toBeCalledTimes(2);
   });
 });
