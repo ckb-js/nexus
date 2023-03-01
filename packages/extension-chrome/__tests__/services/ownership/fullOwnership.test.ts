0b4d1a9eb388eeafaef22daf0d49e695139274b0
zhangyouxin <zhangyouxingg@126.com>
2023-03-01 10:52:48 +0800

    test: add fullOwnership test cases

--- packages/extension-chrome/__tests__/services/ownership/fullOwnership.test.ts
+++ packages/extension-chrome/__tests__/services/ownership/fullOwnership.test.ts
@@ -1,23 +1,35 @@
-import { createFullOwnershipService, createWatchtower } from '../../../src/services/ownership';
+import { config } from '@ckb-lumos/lumos';
+import { Backend } from './../../../src/services/ownership/backend';
+import { TransactionSkeletonType } from '@ckb-lumos/helpers';
+import { TransactionSkeleton } from '@ckb-lumos/helpers';
+import {
+  createFullOwnershipService,
+  createWatchtower,
+  FULL_OWNERSHIP_EXTERNAL_PARENT_PATH,
+  FULL_OWNERSHIP_INTERNAL_PARENT_PATH,
+} from '../../../src/services/ownership';
 import { createMockModule } from '../../helpers/createMockModule';
-import { KeystoreService } from '@nexus-wallet/types';
-import { createScriptInfoDb } from '../../../src/services/ownership/storage';
+import { KeystoreService, OwnershipService } from '@nexus-wallet/types';
+import { createScriptInfoDb, LockStatus, ScriptInfo } from '../../../src/services/ownership/storage';
 import { createConfigService } from '../../../src/services/config';
 import { createMockStorage } from '../../helpers/mockStorage';
 import { asyncSleep } from '../../helpers/utils';
 import { mockBackend } from './mockBackend';
 import { mockPlatformService } from '../../helpers';
 import { bytes } from '@ckb-lumos/codec';
+import { Script, utils, Transaction } from '@ckb-lumos/lumos';
+import { common } from '@ckb-lumos/common-scripts';
 
 describe('FullOwnership', () => {
   describe('getOffChainLocks', function () {
-    it('should get the first 20 locks of the external full-ownership', async () => {
+    let ownershipService: OwnershipService;
+    beforeAll(async () => {
       const storage = createMockStorage();
       const configService = createConfigService({ storage });
       const keystoreService = createMockModule<KeystoreService>({
         getPublicKeyByPath: () => bytes.hexify(Buffer.alloc(33)),
       });
-      const ownershipService = createFullOwnershipService({
+      ownershipService = createFullOwnershipService({
         configService,
         keystoreService,
         platformService: mockPlatformService,
@@ -31,11 +43,147 @@ describe('FullOwnership', () => {
       watchtower.run();
       await asyncSleep(100);
       watchtower.stop();
-
+    });
+    it('should get the first 20 locks of the external full-ownership', async () => {
       const locks = await ownershipService.getOffChainLocks({});
       expect(locks).toHaveLength(20);
     });
 
-    it.skip('should get the first 20 locks of the internal full-ownership', () => {});
+    it('should get the first 20 locks of the internal full-ownership', async () => {
+      const locks = await ownershipService.getOffChainLocks({ change: 'internal' });
+      expect(locks).toHaveLength(20);
+    });
+  });
+
+  describe('getOnChainLocks, signData and signTx', function () {
+    let ownershipService: OwnershipService;
+    let keystoreService: KeystoreService;
+    const scriptInfos: ScriptInfo[] = [
+      createScriptInfo(1, '0x01', 'external', 'OffChain'),
+      createScriptInfo(2, '0x02', 'external', 'OnChain'),
+      createScriptInfo(3, '0x03', 'internal', 'OffChain'),
+      createScriptInfo(4, '0x04', 'internal', 'OnChain'),
+    ];
+    beforeAll(async () => {
+      const storage = createMockStorage();
+      const configService = createConfigService({ storage });
+      keystoreService = createMockModule<KeystoreService>({
+        getPublicKeyByPath: () => bytes.hexify(Buffer.alloc(33)),
+        signMessage: jest.fn().mockImplementation(() => Promise.resolve('0x')),
+      });
+      ownershipService = createFullOwnershipService({
+        configService,
+        keystoreService,
+        platformService: mockPlatformService,
+        storage,
+        backendProvider: {
+          resolve: () =>
+            ({
+              resolveTx: () => {
+                return Promise.resolve({});
+              },
+              getSecp256k1Blake160ScriptConfig: () => {
+                return Promise.resolve(config.predefined.AGGRON4.SCRIPTS.SECP256K1_BLAKE160);
+              },
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            } as any as Backend),
+        },
+      });
+      const db = createScriptInfoDb({ storage, networkId: (await configService.getSelectedNetwork()).id });
+      await db.setAll(scriptInfos);
+    });
+    it('should get the on-chain external locks of full-ownership', async () => {
+      await expect(ownershipService.getOnChainLocks({})).resolves.toEqual({
+        cursor: '2',
+        objects: [scriptInfos[1].lock],
+      });
+    });
+    it('should get the on-chain internal locks of full-ownership', async () => {
+      await expect(ownershipService.getOnChainLocks({ change: 'internal' })).resolves.toEqual({
+        cursor: '4',
+        objects: [scriptInfos[3].lock],
+      });
+    });
+
+    it('should signData by keystore service with proper params', async () => {
+      await ownershipService.signData({ data: '0x1234', lock: scriptInfos[0].lock });
+      expect(keystoreService.signMessage).toBeCalledWith({
+        message: '0x1234',
+        password: '12345678',
+        path: `${scriptInfos[0].parentPath}/${scriptInfos[0].childIndex}`,
+      });
+      jest.clearAllMocks();
+    });
+
+    it('should signTx by keystore service with proper params', async () => {
+      jest.spyOn(common, 'prepareSigningEntries').mockImplementation(() => createMockTxSkeleton());
+      await ownershipService.signTransaction({ tx: {} as Transaction });
+      expect(keystoreService.signMessage).toHaveBeenCalledTimes(2);
+      expect(keystoreService.signMessage).nthCalledWith(1, {
+        message: '0x1234',
+        password: '12345678',
+        path: `${scriptInfos[0].parentPath}/${scriptInfos[0].childIndex}`,
+      });
+      expect(keystoreService.signMessage).nthCalledWith(2, {
+        message: '0x5678',
+        password: '12345678',
+        path: `${scriptInfos[1].parentPath}/${scriptInfos[1].childIndex}`,
+      });
+      jest.clearAllMocks();
+    });
   });
 });
+
+function createScriptInfo(id: number, args: string, change: 'external' | 'internal', status: LockStatus): ScriptInfo {
+  const lock: Script = {
+    codeHash: bytes.hexify(Buffer.alloc(32)),
+    hashType: 'type',
+    args,
+  };
+  return {
+    id,
+    lock,
+    publicKey: '0x',
+    parentPath: change === 'external' ? FULL_OWNERSHIP_EXTERNAL_PARENT_PATH : FULL_OWNERSHIP_INTERNAL_PARENT_PATH,
+    childIndex: 0,
+    status,
+    scriptHash: utils.computeScriptHash(lock),
+  };
+}
+
+function createMockTxSkeleton(): TransactionSkeletonType {
+  let txSkeleton = TransactionSkeleton();
+  txSkeleton = txSkeleton.update('inputs', (inputs) => {
+    return inputs.push(
+      {
+        cellOutput: {
+          capacity: '0x100000000',
+          lock: createScriptInfo(1, '0x01', 'external', 'OnChain').lock,
+        },
+        data: '0x',
+      },
+      {
+        cellOutput: {
+          capacity: '0x100000000',
+          lock: createScriptInfo(2, '0x02', 'external', 'OnChain').lock,
+        },
+        data: '0x',
+      },
+    );
+  });
+  txSkeleton = txSkeleton.update('signingEntries', (signingEntries) => {
+    return signingEntries.push(
+      {
+        index: 0,
+        message: '0x1234',
+        type: '',
+      },
+      {
+        index: 1,
+        message: '0x5678',
+        type: '',
+      },
+    );
+  });
+  return txSkeleton;
+}
