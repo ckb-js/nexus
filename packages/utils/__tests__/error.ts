1177530a5369bf111a1a62554f4d677d2c4fd3a6
homura <homura.dev@gmail.com>
2023-03-08 22:20:46 +0800

    feat: support log object in logger
    Signed-off-by: homura <homura.dev@gmail.com>

--- packages/utils/__tests__/error.ts
+++ packages/utils/__tests__/error.ts
@@ -6,7 +6,7 @@ describe('errors', () => {
   });
 
   test('format error message', () => {
-    expect(() => errors.throwError('error string: %s', 'message')).toThrow(/error string: message /);
+    expect(() => errors.throwError('error string: %s', 'message')).toThrow(/error string: message/);
     expect(() => errors.throwError('error buffer: %s', Buffer.from([0, 1, 2]))).toThrow(/error buffer: 0x000102/);
     expect(() => errors.throwError('error number: %s', 1)).toThrow(/error number: 1/);
     expect(() => errors.throwError('error object: %s', { key: 'value' })).toThrow(/error object: {"key":"value"}/);
