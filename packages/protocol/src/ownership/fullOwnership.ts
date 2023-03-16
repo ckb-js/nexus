518b7e97125318bd7852e0aa7b8a7ceddf07e33a
zhangyouxin <zhangyouxingg@126.com>
2023-03-16 08:50:27 +0800

    refactor: move requester info to base

--- packages/protocol/src/ownership/fullOwnership.ts
+++ packages/protocol/src/ownership/fullOwnership.ts
@@ -1,14 +1,10 @@
-import type { Cell, Cursor, HexString, Paginate, Script, Signature, Transaction } from '../base';
+import type { Cell, Cursor, HexString, Paginate, RequesterInfo, Script, Signature, Transaction } from '../base';
 
 /**
  * "Nervos Message:" in ASCII, used as a magic prefix when signing personal messages.
  * @see {@link https://github.com/nervosnetwork/neuron/blob/1f19cca93b3247447ab6082eca16021dd6656db6/packages/neuron-wallet/src/services/sign-message.ts#L14 Neuron implementation}
  */
 export const SIGN_DATA_MAGIC = '0x4e6572766f73204d6573736167653a';
-
-export type RequesterInfo = {
-  url: string;
-};
 
 export type SignDataPayload = RequesterInfo & {
   data: HexString;
