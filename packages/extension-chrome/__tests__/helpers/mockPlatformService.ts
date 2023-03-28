76b6da448a4d3361b528843eec517a8187b2282c
IronLu233 <ironlu233.dev@outlook.com>
2023-03-28 13:57:32 +0800

    feat(extension-chrome): try fix coverage

--- packages/extension-chrome/__tests__/helpers/mockPlatformService.ts
+++ packages/extension-chrome/__tests__/helpers/mockPlatformService.ts
@@ -4,10 +4,10 @@ export const MOCK_PLATFORM_URL = 'internal://nexus-wallet.io';
 export const MOCK_PLATFORM_PASSWORD = '12345678';
 
 export const mockPlatformService: PlatformService = /* istanbul ignore next */ {
+  getActiveSiteInfo: async () => undefined,
   getRequesterAppInfo: async () => ({ url: MOCK_PLATFORM_URL }),
   requestGrant: async () => {},
   requestSignData: async () => ({ password: MOCK_PLATFORM_PASSWORD }),
   requestSignTransaction: async () => ({ password: MOCK_PLATFORM_PASSWORD }),
   navigateToInitWallet: async () => {},
-  getActiveSiteInfo: async () => undefined,
 };
