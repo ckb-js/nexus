f15f0751c485b9205df919e463028ddfc479d69d
gpBlockchain <744158715@qq.com>
2023-03-28 17:58:45 +0800

    style(e2e): remove deprecated RPC interfaces

--- e2e/packages/e2e/src/setup/launch.ts
+++ e2e/packages/e2e/src/setup/launch.ts
@@ -2,7 +2,6 @@ import { NexusLaunchOptions } from '../types';
 
 import { chromium } from 'playwright-core';
 import { BrowserContext } from 'playwright';
-import { PreloadJsContext } from '../nexus/servicer/provider';
 
 export async function launchWithNexus(option: NexusLaunchOptions, userDataDir = 'tmp/nexus'): Promise<BrowserContext> {
   console.log(`userDataDir:${userDataDir}`);
@@ -11,13 +10,8 @@ export async function launchWithNexus(option: NexusLaunchOptions, userDataDir = 
     args: [`--disable-extensions-except=${option.nexusPath}`, `--load-extension=${option.nexusPath}`],
     permissions: ['clipboard-read'],
     slowMo: 10,
-    // // recordVideo: {
-    // //     dir: 'videos/',
-    // //     size: { width: 640, height: 480 },
-    // // }
     ...option.playwrightOptions,
   });
-  await browserContext.addInitScript({ content: PreloadJsContext });
   browserContext.setDefaultTimeout(10000);
   return browserContext;
 }
