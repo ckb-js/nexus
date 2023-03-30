68abe02fd7c89f89b1b4dcbaabdd0e86dfa390a6
gpBlockchain <744158715@qq.com>
2023-03-30 13:39:12 +0800

    fix: test timeout 3min

--- e2e/packages/e2e/jest.config.js
+++ e2e/packages/e2e/jest.config.js
@@ -7,15 +7,14 @@ module.exports = {
     // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports.
     // See https://github.com/uuidjs/uuid/issues/451
     uuid: require.resolve('uuid'),
-
-    // '@vespaiach/axios-fetch-adapter': '<rootDir>/__mocks__/axiosAdapter.js',
   },
   reporters: ['default', 'jest-allure'],
   testRunner: 'jest-jasmine2',
   testMatch: ['**/__tests__/**/*.test.ts'],
   setupFiles: ['<rootDir>/__tests__/setup/setUp.ts'],
   setupFilesAfterEnv: ['jest-allure/dist/setup'],
-  testTimeout: 3000000,
+  // 3min
+  testTimeout: 180000,
   // setupFiles: ['./setupJest.js'],
   // coveragePathIgnorePatterns: ['thirdpartyLib'],
   maxWorkers: 1,
