4f85ae497b39b5c4aa806328f89f5a5102cb0aca
homura <homura.dev@gmail.com>
2023-04-11 11:27:38 +0800

    test: add falsy cases for assert

--- packages/utils/__tests__/asserts.ts
+++ packages/utils/__tests__/asserts.ts
@@ -3,4 +3,12 @@ import { assert } from '../src';
 it('utils#asserts', () => {
   expect(() => assert(1 + 1 === 2)).not.toThrow();
   expect(() => assert(1 + 1 === 3)).toThrow();
+
+  expect(() => assert(0)).toThrow();
+  expect(() => assert('')).toThrow();
+  expect(() => assert(undefined)).toThrow();
+  expect(() => assert(null)).toThrow();
+
+  expect(() => assert(1)).not.toThrow();
+  expect(() => assert('1')).not.toThrow();
 });
