5fc4b91f5b88b26263d3c08f5764dcc3c3723174
gpBlockchain <744158715@qq.com>
2023-04-18 11:55:33 +0800

    test: add account-detail test

---
+++ e2e/tests/account-detail.test.ts
@@ -0,0 +1,43 @@
+import { DefaultTestEnv } from '../helpers';
+
+DefaultTestEnv.setupTest({ initWalletWithDefaults: true });
+
+describe('Show account details', function () {
+  it('should show nick name for account', async () => {
+    const page = await testEnv.context.newPage();
+    const extensionId = testEnv.extensionId;
+    await page.goto(`chrome-extension://${extensionId}/popup.html`);
+    await page.getByText(testEnv.defaultE2eData.nickname).waitFor();
+  });
+
+  it('should show disconnected for the account', async () => {
+    const page = await testEnv.context.newPage();
+    const extensionId = testEnv.extensionId;
+    await page.goto(`chrome-extension://${extensionId}/popup.html`);
+    await page.getByText('Disconnected').waitFor();
+  });
+  it('should show whitelist sites for the account', async () => {
+    const page = await testEnv.context.newPage();
+    const extensionId = testEnv.extensionId;
+    await page.goto(`chrome-extension://${extensionId}/popup.html`);
+    await page.getByRole('button', { name: 'Whitelist Sites' }).click();
+    await page.getByText('No whitelist sites found.').waitFor();
+  });
+  it('should show networks list for the account', async () => {
+    const page = await testEnv.context.newPage();
+    const extensionId = testEnv.extensionId;
+    await page.goto(`chrome-extension://${extensionId}/popup.html`);
+    await page.getByRole('button', { name: 'Network' }).click();
+    await page.getByText('Mainnet').waitFor();
+    await page.getByText('Testnet').waitFor();
+  });
+
+  it('should show feedback for the account', async () => {
+    const page = await testEnv.context.newPage();
+    const extensionId = testEnv.extensionId;
+    await page.goto(`chrome-extension://${extensionId}/popup.html`);
+    await page.getByRole('link', { name: 'Feedback' }).click();
+    await page.waitForTimeout(1000);
+    expect(testEnv.context.pages().some((page) => page.url().includes('ckb-js/nexus/issues'))).toBe(true);
+  });
+});
